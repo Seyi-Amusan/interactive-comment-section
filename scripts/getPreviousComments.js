ec2c569d08e2ff1fbaebb00924b407b2509b6c71
Seyi Amusan <seyijoshua14@gmail.comgit config --global user.email seyijoshua14@gmail.com>
2024-05-30 19:39:44 +0100

    Started writing code to generate previous comments and fixed layout of reply comments

--- scripts/getPreviousComments.js
+++ scripts/getPreviousComments.js
@@ -3,16 +3,29 @@ import { section, cloneCommentDiv } from "./index.js";
 async function loadCommentsFromJson() {
     const result = await fetch('../data.json')
     const data = await result.json()
-    displayComments(data)
+    displayCommentsNode(data)
 }
 
+let i = 1
 
-function displayComments(data) {
+
+function displayCommentsNode(data) {
     data.comments.forEach(comment => {
         
         //displays a comment div
         const newCommentDiv = cloneCommentDiv()
         section.append(newCommentDiv)
+
+        //input the correct data from data.json into the node
+        
+        while (i > -1) {
+
+            inputCommentNodeData(data, newCommentDiv, i)
+            i--
+
+
+        }
+        
 
 
         //checks if there are replies to the comment div
@@ -29,6 +42,25 @@ function displayComments(data) {
     });
 }
 
+
+
+function inputCommentNodeData(data, node, index) {
+    console.log(index);
+    const header = node.querySelector('.header').children
+
+    const commentData = data.comments[index] //this is declared to minimize repetition
+    
+    header[0].setAttribute('src', commentData.user.image.png) //changes the avatar
+    header[1].innerText = commentData.user.username //changes the username
+    header[2].innerText = commentData.createdAt //changes the time on comment
+
+    node.querySelector('.body .content').innerText = commentData.content //changes the user comment
+
+    node.querySelector('.footer .score p').innerText = commentData.score //changes the score
+
+
+    console.log(commentData);
+}
 
 loadCommentsFromJson()
 
